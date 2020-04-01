acdd264da138bace67e3757dca2b9eb058306e82
Uliana Dzyoba <ulianadzyoba@gmail.com>
2020-04-01 20:14:58 +0300

    event for form

--- lab4/task1/main.js
+++ lab4/task1/main.js
@@ -1,24 +1,8 @@
 function setUp(){
   const form=document.getElementById('img-form');
   const inputs=form.getElementsByTagName('input');
-  for(let i=0; i<inputs.length; i++){
-    inputs[i].addEventListener('input', validateInput);
-
-  }
+  form.addEventListener('input', validateInput);
   form.addEventListener('submit', validateForm);
-  // form.addEventListener('submit', function(){
-  //   let isValid=true;
-  //   for(let i=0; i<inputs.length; i++){
-  //     if(inputs[i].value==""){
-  //       inputs[i].classList.add('error');
-  //     }
-  //     if(inputs[i].classList.contains('error')) {
-  //       isValid=false;
-  //     }
-  //   } 
-
-  //   if(isValid) changeImage(inputs);
-  // });
 }
 function validateForm(event){
   event.preventDefault();
